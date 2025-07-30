dbad6e269e8a9a546ef720e5fdb1d6e59e877813
Aryam <aryamiyezra@gmail.com>
2025-07-31 01:51:08 +0300

    from xml http req to fetch for load products

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -62,6 +62,30 @@ const date = new Date(); // built in date class
 */
 
 export let products = [];
+export function loadProductsFetch(){
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) =>{
+    // console.log(productsData);
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log(products);
+
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() =>{
+  console.log('next step');
+});
+*/
 
 export function loadProducts(func){
   const xhr = new XMLHttpRequest();
